d9aa90386dcddf3794c0366ca112ae648d8c5463
Uzair Ali <uzairali001@gmail.com>
2025-05-24 23:47:00 +0500

    Revamp, more algo added

--- generate.php
+++ generate.php
@@ -1,26 +1,57 @@
 <?php
 
-    if($_SERVER['REQUEST_METHOD'] !== 'POST'){
-        http_response_code(405); //Method Not Allowed        
-    }
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  http_response_code(405); //Method Not Allowed
+}
 
-    // $rest_json = file_get_contents("php://input");
-    // $_POST = json_decode($rest_json, true);
-    
+// $rest_json = file_get_contents("php://input");
+// $_POST = json_decode($rest_json, true);
 
-    if(!empty($_POST) && !empty($_POST["plain_pass"]))
-    {
-        echo
-        json_encode(
-            array(
-                "hash"=> password_hash($_POST["plain_pass"], PASSWORD_BCRYPT)
-            )
-        );
+$postData = (object)[
+  "plainText" => $_POST["plain-text"],
+  "cost" => $_POST["cost"] ?? 12,
+  "algo" => $_POST["algo"] ?? "bcrypt",
+];
 
-        
-        return;
-    }
+if (!$postData->plainText) {
+  http_response_code(400); //Bad Request
+  return;
+}
 
-    http_response_code(400); //Bad Request     
 
-?>
+
+echo json_encode([
+  "hash" => getHash($postData),
+]);
+
+
+
+/**
+ * Returns a hash of the plaintext password, using the given algorithm.
+ *
+ * @param object $postData
+ *   Plain text password to be hashed, cost of the hash, and algorithm to use.
+ *   Algorithm can be 'bcrypt', 'md5', 'sha1', 'sha256', or 'sha512'.
+ *
+ * @return string
+ *   Hashed password.
+ *
+ * @throws Exception
+ *   If the algorithm is invalid.
+ */
+function getHash($postData)
+{
+  if ($postData->algo === "bcrypt") {
+    $options = [
+      'cost' => $postData->cost ?? 12,
+    ];
+    return password_hash($postData->plainText, PASSWORD_BCRYPT, $options);
+  } else if ($postData->algo === "md5") {
+    return md5($postData->plainText);
+  } else if (in_array($postData->algo, ["sha1", "sha256", "sha512", "sha3-256", "sha3-512"])) {
+    return hash($postData->algo, $postData->plainText);
+  }
+
+
+  throw new Exception("Invalid algorithm");
+}
--- index.php
+++ index.php
@@ -1,47 +1,63 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <title>PHP Password Generator</title>
-    <link rel="stylesheet" href="password/assets/css/reset.css">
-    <link rel="stylesheet" href="password/assets/css/styles.css">
-    
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet">
+  <title>Password Hash</title>
+  <link rel="stylesheet" href="assets/css/reset.css">
+  <link rel="stylesheet" href="assets/css/styles.css">
+
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700&display=swap" rel="stylesheet">
+
 <body>
-    <main class="main-container">
-        <div class="heading-container">
-            <h1>PHP Password Generator</h1>
-        </div>
-        <div class="form-container">
-            <form class="myform" action="generate.php" method="post" id="pass-generate-form">
-                <div class="blur"></div>
-                <div class="content-container">
-                    <div class="field-container plain-password-container">
-                        <label for="plain-password">Plain Password</label>
-                        <input id="plain-password" class="input" name="plain-password" type="text">
-                    </div>
-                    
-                    <!-- <php echo password_hash("agp@123", PASSWORD_BCRYPT); ?> -->
-                    <div class="button-container">
-                        <button class="btn red generate-btn" type="submit">Generate</button>
-                    </div>
+  <main class="main-container">
+    <div class="heading-container">
+      <h1>Password Hash</h1>
+    </div>
+    <div class="form-container">
+      <form class="myform" action="generate.php" method="post" id="form">
+        <div class="content-container">
 
-                    <div class="field-container encrypted-password-container" data-js="ec_pass_cont">
-                        <label for="">Encrypted Password</label>
-                        <div id="encryptedpass-text" class="encryptedpass-text display-ib muted-invert fw-light">Generated Password will be here</div>
-                    </div>
-                </div>
-                
-            </form>
+          <div class="field-container">
+            <label for="algo">Algorithm</label>
+            <select class="select-field" name="algo">
+              <option value="bcrypt">Bcrypt</option>
+              <option value="md5">MD5</option>
+              <option value="sha1">SHA1</option>
+              <option value="sha256">SHA256</option>
+              <option value="sha512">SHA512</option>
+              <option value="sha3-256">SHA3-256</option>
+              <option value="sha3-512">SHA3-512</option>
+            </select>
+          </div>
+
+          <div class="field-container">
+            <label for="plain-text">Plain Password</label>
+            <input id="plain-text" class="input-field" name="plain-text" type="password">
+          </div>
+
+          <!-- <php echo password_hash("agp@123", PASSWORD_BCRYPT); ?> -->
+          <div class="button-container">
+            <button class="btn red generate-btn" type="submit">Generate Hash</button>
+          </div>
+
+          <div class="field-container encrypted-password-container" data-js="ec_pass_cont">
+            <label for="">Hashed Password</label>
+            <div id="encryptedpass-text" class="encryptedpass-text display-ib muted-invert fw-light">Generated hash will be here</div>
+          </div>
         </div>
-    </main>
+
+      </form>
+    </div>
+  </main>
 
   <section class="toast-container">
   </section>
 
-    <script src="password/assets/scripts/main.js"></script>
+  <script src="assets/scripts/main.js"></script>
 </body>
+
 </html>
